6e84af5f8cea197d18be4d5b490750e6f9a92670
Eduardo Cipriano <edfcsx@gmail.com>
2021-01-19 15:36:02 -0300

    initial files

---
+++ src/main/java/com/edfcsx/course/resources/UserResource.java
@@ -0,0 +1,27 @@
+package com.edfcsx.course.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.edfcsx.course.entities.User;
+import com.edfcsx.course.services.UserService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@Autowired
+	private UserService userService;
+	
+	@GetMapping
+	public ResponseEntity<List<User>> findAll() {
+		List<User> users = userService.findAll();
+		
+		return ResponseEntity.ok().body(users);
+	}
+}
