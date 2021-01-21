949c432e1b23f416e16eebc9eed22dfd8ea28670
Silva, Eduardo Felipe <eduardo.felipe.silva@accenture.com>
2021-01-21 12:02:01 -0300

    Foram criadas as associações entre categorias e produtos

--- src/main/java/com/edfcsx/course/entities/Product.java
+++ src/main/java/com/edfcsx/course/entities/Product.java
@@ -20,9 +20,14 @@ public class Product implements Serializable {
     private Double price;
     private String imgUrl;
 
-    // Set funciona igual uma List, porem nao permite elementos duplicados
-    @Transient
+    @ManyToMany
+    @JoinTable(
+        name = "tb_product_category",
+        joinColumns = @JoinColumn(name = "product_id"),
+        inverseJoinColumns = @JoinColumn(name = "category_id")
+    )
     private Set<Category> categories = new HashSet<>();
+    // Set funciona igual uma List, porem nao permite elementos duplicados
 
     public Product() {}
 
