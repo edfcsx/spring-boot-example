f868adf496c89b4ded9c787d64bd23b0efd70f0a
Silva, Eduardo Felipe <eduardo.felipe.silva@accenture.com>
2021-02-01 16:46:01 -0300

    Foi adicionada uma exceção no update do usuario

--- src/main/java/com/edfcsx/course/services/UserService.java
+++ src/main/java/com/edfcsx/course/services/UserService.java
@@ -14,6 +14,8 @@ import org.springframework.stereotype.Service;
 import com.edfcsx.course.entities.User;
 import com.edfcsx.course.repositories.UserRepository;
 
+import javax.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -45,10 +47,14 @@ public class UserService {
 	}
 
 	public User update(Long id, User obj) {
-		User entity = repository.getOne(id);
-		updateData(entity, obj);
+		try {
+			User entity = repository.getOne(id);
+			updateData(entity, obj);
 
-		return repository.save(entity);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
