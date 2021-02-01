8fb562047895397548d635d0a4272a258f4cd8cc
Silva, Eduardo Felipe <eduardo.felipe.silva@accenture.com>
2021-02-01 10:51:36 -0300

    Foi implementado o recurso de exclusão de usuários

--- src/main/java/com/edfcsx/course/resources/UserResource.java
+++ src/main/java/com/edfcsx/course/resources/UserResource.java
@@ -51,4 +51,11 @@ public class UserResource {
 
 		return ResponseEntity.noContent().build();
 	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj) {
+		obj = userService.update(id, obj);
+
+		return ResponseEntity.ok().body(obj);
+	}
 }
